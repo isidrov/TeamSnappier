edc00e1ede8977255f9f980c5917f75e6d65e379
“isidrov” <isidrov@gmail.com>
2023-09-23 10:06:06 -0400

    sept 23

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def find_me():
 
     if response.status_code == 200:
 
-        print("Request was successful!")
+        print("find_me() was successful!\n")
 
         parsed_json = response.json()
         user_id = parsed_json["collection"]["items"][0]["data"][00]["value"]
@@ -47,7 +47,7 @@ def get_url(url):
 
     if response.status_code == 200:
 
-        print("Request was successful!")
+        print("get_url() was successful!\n")
 
         parsed_json = response.json()
 
@@ -57,60 +57,20 @@ def get_url(url):
         print(f"Request failed with status code: {response.status_code}")
         print(response.text)
 
-def members():
-
-    API_HREF = "https://api.teamsnap.com/v3/members"  # Replace with your endpoint URL
-
-    response = requests.get(API_HREF, headers=headers)
-
-    if response.status_code == 200:
-
-        print("Request was successful!")
-        parsed_json = response.json()
-
-        print(json.dumps(parsed_json["collection"]["items"][0]["data"], indent=4))
-
-        user_id = parsed_json["collection"]["items"][0]["data"][00]["value"]
-
-        return user_id
-
-        #for item in parsed_json["collection"]["items"][0]["data"]:
-            #print(json.dumps(item, indent=4))
-
-    else:
-        print(f"Request failed with status code: {response.status_code}")
-        print(response.text)
-
-def divisions():
-
-    API_HREF = "https://api.teamsnap.com/v3/divisions"  # Replace with your endpoint URL
-
-    response = requests.get(API_HREF, headers=headers)
-
-    if response.status_code == 200:
-
-        print("Request was successful!")
-        parsed_json = response.json()
-
-        for division in parsed_json:
-            print(division['name'])
-
-        #for item in parsed_json["collection"]["items"][0]["data"]:
-            #print(json.dumps(item, indent=4))
-
-    else:
-        print(f"Request failed with status code: {response.status_code}")
-        print(response.text)
 
 def list_teams(userid):
 
-    API_HREF = f"https://api.teamsnap.com/v3/teams/search?user_id={userid}"
-    print(API_HREF)
-    response = requests.get(API_HREF, headers=headers)
+    params = {
+        'user_id': userid
+    }
+
+    API_HREF = f"https://api.teamsnap.com/v3/teams/search"
+
+    response = requests.get(API_HREF, headers=headers, params=params)
 
     if response.status_code == 200:
 
-        print("Request was successful!")
+        print("list_teams() was successful!\n")
         parsed_json = response.json()
 
         list_of_teams = []
@@ -125,8 +85,6 @@ def list_teams(userid):
 
         return list_of_teams
 
-        #for item in parsed_json["collection"]["items"][0]["data"]:
-            #print(json.dumps(item, indent=4))
 
     else:
         print(f"Request failed with status code: {response.status_code}")
@@ -134,16 +92,24 @@ def list_teams(userid):
 
 def search_user(userid):
 
-    API_HREF = f"https://apiv3.teamsnap.com/users/search?id={userid}"  # Replace with your endpoint URL
+    params = {
+        'id': userid
+    }
 
-    response = requests.get(API_HREF, headers=headers)
+    API_HREF = f"https://apiv3.teamsnap.com/users/search"  # Replace with your endpoint URL
+
+    response = requests.get(API_HREF, headers=headers, params=params)
 
     if response.status_code == 200:
 
-        print("Request was successful!")
+        print("search_user() was successful!\n")
+
         parsed_json = response.json()
 
-        print(f"My ID is {parsed_json['collection']['items'][0]['data'][00]['value']}")
+        print(f"id: is {parsed_json['collection']['items'][0]['data'][0]['value']}")
+        print(f"email: {parsed_json['collection']['items'][0]['data'][5]['value']}")
+        print(f"First Name: {parsed_json['collection']['items'][0]['data'][8]['value']}")
+        print(f"Last Name: {parsed_json['collection']['items'][0]['data'][10]['value']}\n")
 
     else:
         print(f"Request failed with status code: {response.status_code}")
@@ -156,6 +122,26 @@ def create_team_member(member):
 
     response = requests.post(API_HREF, headers=headers,json=member)
 
+    if response.status_code in (200, 201, 204):
+
+        print(f"{response.status_code} request successful!")
+
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+        print(response.text)
+
+#https://api.teamsnap.com/v3/events/search?user_id=28819952'
+
+def list_events(userid):
+
+    params = {
+        'id': userid
+    }
+
+    API_HREF = f"https://apiv3.teamsnap.com/users/search"  # Replace with your endpoint URL
+
+    response = requests.get(API_HREF, headers=headers, params=params)
+
     if response.status_code == 200:
 
         print("Request was successful!")
@@ -167,18 +153,41 @@ def create_team_member(member):
         print(f"Request failed with status code: {response.status_code}")
         print(response.text)
 
+def list_members(team_id):
+
+    params = {
+        'team_id': team_id
+    }
+
+    API_HREF = f"https://api.teamsnap.com/v3/members/search"  # Replace with your endpoint URL
+
+    response = requests.get(API_HREF, headers=headers, params=params)
+
+    if response.status_code == 200:
+
+        print("list_members() was successful!")
+        parsed_json = response.json()
+
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+        print(response.text)
+
 def main():
 
     userid = find_me()
 
-    #search_user(str(userid))
+    search_user(userid)
 
-    teams_I_belong = list_teams(str(userid))
+    teams_I_belong = list_teams(userid)
 
     for team in teams_I_belong:
         print(f"Team Name: {team['name']}\nTeam Id: {team['id']}")
         print("\n")
 
+    list_members(8780519)
+
+
+    '''
     data = {
         "template": {
             "data": [
@@ -187,9 +196,9 @@ def main():
                 {"name": "team_id", "value": 8780519}
             ]
         }
-    }
+    }'''
 
-    create_team_member(data)
+    #create_team_member(data)
     #get_url("https://api.teamsnap.com/v3/events")
 
 
